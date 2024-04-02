bc69800e3295c2fb405d3d747a10607ab0248fc0
Angelina Tsai <111481894+angelinatsai04@users.noreply.github.com>
2024-04-02 08:07:07 -0700

    cart button

--- src/App.js
+++ src/App.js
@@ -6,25 +6,34 @@ function Main() {
   const [curState, setCurState] = useState();
   const [cart, setCart] = useState(0);
   const [search, setSearch] = useState('')
+  const [cartToggle, setCartToggle] = useState(false); 
   // const [products, setProducts] = useState([]);
   console.log("hi");
 
   async function PopulateData() {
-      const response = await fetch('https://dummyjson.com/products');
+      const response = await fetch(`https://dummyjson.com/products/search?q=${search}`);
       const data = await response.json();
       setCurState(data);
   }
-  
+
   useEffect(() => {
     PopulateData();
-  }, []);
+  }, [search]);
 
   
   // console.log(curState.products[0]);
+  console.log(`https://dummyjson.com/products/search?q=${search}`);
+  console.log({search});
   console.log("bye");
 
   return (
     <div>
+      <button onClick={() => setCartToggle(!cartToggle)}>
+        {!cartToggle
+        ? "Click on Cart"
+        : cart}
+        {/* if quantity != displaay in cart */}
+      </button>
       <input type="text" placeholder="Search products..." value={search} onChange={(e) => setSearch(e.target.value)} />
       <div>
         {!curState 
@@ -32,6 +41,7 @@ function Main() {
         : curState.products.map((product) => <ItemDisplay product={product} cart={cart} setCart={setCart} />)
         }
       </div>
+      
     </div>
   );
 }
